701b4bff12e4f0ed2fc427975e18990365b7529b
Duma_Tabby <tabithaondego9@gmail.com>
2026-04-09 20:05:50 -0700

    new

--- app/src/main/java/com/example/wellnessapp/NutritionActivity.kt
+++ app/src/main/java/com/example/wellnessapp/NutritionActivity.kt
@@ -1,20 +1,71 @@
 package com.example.wellnessapp
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 class NutritionActivity : AppCompatActivity() {
+
+    var rewardedAd: RewardedAd? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_nutrition)
+//        invoke the function to load the reward ad
+        loadRewardedAd(this)
+
+//        find the button by use of its id
+        var rewardAdButton = findViewById<Button>(R.id.rewardAd)
+        rewardAdButton.setOnClickListener {
+            showRewardedAd(this)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun loadRewardedAd(context: Context) {
+        val adRequest = AdRequest.Builder().build()
+
+        RewardedAd.load(context,
+            "ca-app-pub-3940256099942544/5224354917",
+            adRequest,
+            object : RewardedAdLoadCallback() {
+
+                override fun onAdLoaded(ad: RewardedAd) {
+                    rewardedAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    rewardedAd = null
+                }
+            })
+    }
+
+    fun showRewardedAd(activity: Activity) {
+        if (rewardedAd != null) {
+            rewardedAd?.show(activity) { rewardItem ->
+                // Give reward to user
+                val rewardAmount = rewardItem.amount
+                val rewardType = rewardItem.type
+
+                println("User earned: $rewardAmount $rewardType")
+            }
+        } else {
+            println("Ad not ready")
         }
     }
 }
